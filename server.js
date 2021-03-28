23fa4dc8aa2de1ae1187e11d10ffcc2fe44de6c8
Benedikt Dornauer <2010738480@stud.fh-kufstein.ac.at>
2021-03-28 09:34:03 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const helmet = require('helmet');
+
+const app = express();
+const port = process.env.PORT |3000;
+
+
+app.use(cors());
+app.use(helmet());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.post('/addTestData', (req, res) => {
+
+    let data = require('./tests.json');
+    data.push(req.body);
+    const jsonString = JSON.stringify(data)
+
+    fs.writeFile('./tests.json', jsonString, err => {
+        if (err) {
+            console.log('Error writing file', err)
+        } else {
+            console.log('Successfully wrote file')
+        }
+    })
+
+    res.send('Test was added. Thank you');
+});
+
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
